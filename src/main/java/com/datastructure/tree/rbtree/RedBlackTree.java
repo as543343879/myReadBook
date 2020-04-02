7f162911b72879d02bd952161407033cb44ebbe8
as543343879 <543343879@qq.com>
2020-04-02 17:25:55 +0800

    红黑树 部分

--- src/main/java/com/datastructure/tree/rbtree/RedBlackTree.java
+++ src/main/java/com/datastructure/tree/rbtree/RedBlackTree.java
@@ -36,6 +36,107 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
         if(node == null) return false;
         return node.color == RED;
     }
+
+    public void moveflipColors(Node node) {
+        node.color = BLACK;
+        node.left.color = RED;
+        node.right.color = RED;
+    }
+
+    private Node balance(Node h){
+        if (isRed(h.right)) h = rotateLeft(h);
+        if (isRed(h.right) && !isRed(h.left)) h=rotateLeft(h);
+        
+        if (isRed(h.left) && isRed(h.left.left)) h=rotateRight(h);
+        if (isRed(h.left) && isRed(h.right))    flipColors(h);
+        h.N = size(h.left)+size(h.right)+1;
+        return h;
+    }
+
+    private Node moveRedLeft(Node h){
+        /**
+         * 当前节点的左右子节点都是2-节点，左右节点需要从父节点中借一个节点
+         * 如果该节点的右节点的左节点是红色节点，说明兄弟节点不是2-节点，可以从兄弟节点中借一个
+         */
+        moveflipColors(h);     // 从父节点中借一个
+        if(isRed(h.right.left)){    // 判断兄弟节点，如果是非红节点，也从兄弟节点中借一个
+            h.right = rotateRight(h.right);
+            h = rotateLeft(h);
+            moveflipColors(h);  //  在从兄弟节点借了一个以后，我们就需要还一个节点给父节点了，因为一开始从父节点那里借了一个
+        }
+        return h;
+    }
+
+    private Node moveRedRight(Node h){
+        moveflipColors(h);
+        if(isRed(h.left.left)){         // 在这里对于兄弟节点的判断都是.left，因为红色节点只会出现在左边
+            h=rotateRight(h);
+            moveflipColors(h);
+        }
+        return h;
+    }
+
+    public void delete(Key key){
+        if(!isRed(root.left)&& !isRed(root.right)){
+            root.color = RED;
+        }
+        root = delete(root,key);
+        if(root != null)
+            root.color = BLACK;
+    }
+
+    private Node delete(Node h, Key key){
+        if(key.compareTo(h.key) < 0){          // 当目标键小于当前键的时候，我们做类似于寻找最小是的操作，向树左边移动，合并父子结点来消除2-结点
+            if(h.left == null){
+                return null;
+            }
+            if(isRed(h.left) && !isRed(h.left.left)){
+                h = moveRedLeft(h);
+            }
+            h.left = delete(h.left,key);
+        }else{                  // 当目标键大于当前键的时候，我们向右移动，并做与deleteMax相同的操作，如果相同的话，我们使用和二叉树的删除一样的操作，获取当前键的右子树的最小健，然后交换，并将目标键删除
+            if(isRed(h.left)){
+                h = rotateRight(h);
+            }
+            if(key != h.key && h.right == null){    // 我们没有找到目标键，我们将其删除
+                return null;
+            }
+            if(!isRed(h.right) && isRed(h.right.left)){
+                h = moveRedRight(h);
+            }
+            if(key == h.key){
+                h.value = get(h.right,min(h.right).key);
+                h.key = min(h.right).key;
+                h.right = deleteMin(h.right);
+            }
+            else h.right = delete(h.right,key);
+        }
+        return balance(h);
+    }
+
+    /**
+     * 查找以node为根节点红黑树的最小节点
+     * 深度优先遍历，递归实现
+     *
+     * @param node
+     * @return BinarySearchTree<E>.Node
+     * @author ronglexie
+     * @version 2018/8/18
+     */
+    private Node min(Node node) {
+        if(isEmpty()){
+            throw new IllegalArgumentException("BinarySearchTree is empty !");
+        }
+        if(node.left == null){
+            return node;
+        }
+        return min(node.left);
+    }
+
+    private boolean isEmpty() {
+        return root == null;
+    }
+
     public void flipColors(Node node) {
         node.color = RED;
         node.left.color = BLACK;
@@ -79,7 +180,7 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
      * 通过右旋使得左边红节点x 红色传到右边
      * @param node
      */
-    public Node rotaeRight(Node node) {
+    public Node rotateRight(Node node) {
         Node t = node.left;
         node.left = t.right;
         t.right = node;
@@ -115,7 +216,7 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
             node.value = value;
 
         if(isRed(node.right) && !isRed(node.left)) node = rotateLeft(node);
-        if(isRed(node.left) && isRed(node.left.left)) node = rotaeRight(node);
+        if(isRed(node.left) && isRed(node.left.left)) node = rotateRight(node);
         if(isRed(node.left) && isRed(node.right)) flipColors(node);
 
         node.N = size(node.left) + 1 + size(node.right);
