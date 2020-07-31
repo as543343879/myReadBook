7710aee2fe5316900e896c1cce0d899d61a950a0
as543343879 <543343879@qq.com>
2020-07-31 23:17:52 +0800

    最小公共父

--- src/main/java/com/acm/lico/Offer68.java
+++ src/main/java/com/acm/lico/Offer68.java
@@ -1,5 +1,7 @@
 package com.acm.lico;
 
+import com.acm.lico.offer68.TreeNode;
+
 /**
  * Offer68 class
  *
@@ -7,4 +9,27 @@ package com.acm.lico;
  * @date 2020-07-31
  */
 public class Offer68 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 递归到叶子节点，或者 找到pq 直接回溯pq
+        if(root == null || p.val == root.val || q.val == root.val) {
+            return root;
+        }
+
+        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+        TreeNode rigthNode = lowestCommonAncestor(root.right, p, q);
+
+
+        if(leftNode == null && rigthNode == null){
+            return null;
+        }
+
+        if(leftNode == null) {
+            return rigthNode;
+        }
+        if(rigthNode == null) {
+            return leftNode;
+        }
+        //异侧，就回溯 pq 的父节点
+        return root;
+    }
 }
---
+++ src/main/java/com/acm/lico/offer68/TreeNode.java
@@ -0,0 +1,10 @@
+package com.acm.lico.offer68;
+
+
+
+public class TreeNode {
+    public  int val;
+    public  TreeNode left;
+     public TreeNode right;
+    TreeNode(int x) { val = x; }
+}
