b84418623428fc8518fb49237ce1a36bf151da25
as543343879 <543343879@qq.com>
2021-06-26 20:19:47 +0800

    crdt 测试

--- src/main/java/com/datastructure/distributed/CRDTTest.java
+++ src/main/java/com/datastructure/distributed/CRDTTest.java
@@ -1,11 +1,13 @@
 package com.datastructure.distributed;
 
+import com.netopyr.wurmloch.crdt.LWWRegister;
+import com.netopyr.wurmloch.crdt.MVRegister;
 import com.netopyr.wurmloch.crdt.PNCounter;
 import com.netopyr.wurmloch.store.LocalCrdtStore;
-import com.netopyr.wurmloch.crdt.PNCounter;
-import com.netopyr.wurmloch.store.LocalCrdtStore;
-
+import javaslang.collection.Array;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 /**
  * CRDTTest class
@@ -14,6 +16,43 @@ import org.junit.Test;
  * @date 2021-06-26
  */
 public class CRDTTest {
+
+
+    @Test
+    public void testRegisterMV(){
+        LocalCrdtStore crdtStore1 = new LocalCrdtStore("N_1");
+        LocalCrdtStore crdtStore2 = new LocalCrdtStore("N_2");
+        crdtStore1.connect(crdtStore2);
+
+        MVRegister<String> replica1 = crdtStore1.createMVRegister("ID_1");
+
+        MVRegister<String> replica2 = crdtStore2.<String>findMVRegister("ID_1").get();
+
+        replica1.set("xxp");
+        replica2.set("xxp2");
+
+        Array<String> strings = replica1.get();
+        System.out.println(strings.get());
+        System.out.println(replica2.get().equals("banana"));
+
+    }
+
+    @Test
+    public void testRegister(){
+        LocalCrdtStore crdtStore1 = new LocalCrdtStore("N_1");
+        LocalCrdtStore crdtStore2 = new LocalCrdtStore("N_2");
+        crdtStore1.connect(crdtStore2);
+
+        LWWRegister<String> replica1 = crdtStore1.createLWWRegister("ID_1");
+        LWWRegister<String> replica2 = crdtStore2.<String>findLWWRegister("ID_1").get();
+
+        replica1.set("apple");
+        replica2.set("banana");
+
+        System.out.println(replica1.get().equals("banana"));
+        System.out.println(replica2.get().equals("banana"));
+
+    }
     @Test
     public void testPNCount() {
         LocalCrdtStore crdtStore1 = new LocalCrdtStore();
@@ -28,8 +67,7 @@ public class CRDTTest {
 
         System.out.println(replica1.get() == -1L);
         System.out.println(replica2.get() == -1L);
-//        assertThat(replica1.get()).isEqualTo(-1L);
-//        assertThat(replica2.get()).isEqualTo(-1L);
+
 
         crdtStore1.disconnect(crdtStore2);
 
@@ -38,14 +76,12 @@ public class CRDTTest {
 
         System.out.println(replica1.get() == -4L);
         System.out.println(replica2.get() == 4L);
-//        assertThat(replica1.get()).isEqualTo(-4L);
-//        assertThat(replica2.get()).isEqualTo(4L);
+
 
         crdtStore1.connect(crdtStore2);
 
         System.out.println(replica1.get() == 1L);
         System.out.println(replica2.get() == 1L);
-//        assertThat(replica1.get()).isEqualTo(1L);
-//        assertThat(replica2.get()).isEqualTo(1L);
+
     }
 }
