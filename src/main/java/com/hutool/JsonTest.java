a7bbdc808690b5ec7f95e97b08840a439d69ff95
xiexiaoping <xiexiaoping@lizhi.fm>
2021-07-20 18:18:13 +0800

    feat: hutool 测试

---
+++ src/main/java/com/hutool/JsonTest.java
@@ -0,0 +1,13 @@
+package com.hutool;
+
+/**
+ * JsonTest class
+ * https://hutool.cn/docs/#/
+ * https://www.bookstack.cn/read/hutool/871a9e8c60a8b71f.md
+ * @author 谢小平
+ * @date 2021/7/20
+ */
+public class JsonTest {
+    public static void main(String[] args) {
+    }
+}
