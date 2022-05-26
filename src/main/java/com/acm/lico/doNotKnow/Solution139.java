4b9c97fe1889ccee8b4c600977003d3c84f770e6
xiexiaoping <xiexiaoping@lizhi.fm>
2022-05-26 14:55:55 +0800

    feat: 139. 单词拆分

--- src/main/java/com/acm/lico/doNotKnow/Solution139.java
+++ src/main/java/com/acm/lico/doNotKnow/Solution139.java
@@ -32,6 +32,45 @@ import java.util.*;
  */
 public class Solution139 {
     Boolean res_info = false;
+    /**
+     1 复杂度分析
+    估算问题中复杂度的上限和下限
+     时间复杂度
+        O(N^2)
+     空间复杂度
+        O(n) 一次遍历
+
+     2 定位问题
+    根据问题类型，确定采用何种算法思维。
+        动态规划
+        例如
+         这个问题是什么类型（排序、查找、最优化）的问题；
+         这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+         采用哪些数据结构或算法思维，能把这个问题解决。
+     3 数据操作分析
+        根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+     4 编码实现
+     5 执行结果
+     执行结果： 通过 显示详情 添加备注
+     执行用时： 7 ms , 在所有 Java 提交中击败了 59.24% 的用户
+     内存消耗： 41.8 MB , 在所有 Java 提交中击败了 9.58% 的用户 通过测试用例： 45 / 45
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+       Set<String> wordSet = new HashSet<>(wordDict);
+       boolean[] dp = new boolean[s.length() + 1];
+       dp[0]=true;
+       for(int i = 1; i <= s.length(); i ++) {
+           for(int j = 0; j < i; j ++) {
+               if(dp[j] && wordSet.contains(s.substring(j,i))) {
+                    dp[i] = true;
+                    break;
+               }
+
+           }
+       }
+       return dp[s.length() ];
+    }
+
 
     /**
      * 直接模拟 时间超限
@@ -39,7 +78,7 @@ public class Solution139 {
      * @param wordDict
      * @return
      */
-    public boolean wordBreak(String s, List<String> wordDict) {
+    public boolean wordBreak2(String s, List<String> wordDict) {
         Collections.sort(wordDict, (o1, o2) -> o2.length() - o1.length());
         return dfs(s,0,wordDict);
     }
