07152440999ba4bf8c3e1d9e241d5e89bd7dd1ff
xiexiaoping <xiexiaoping@lizhi.fm>
2022-05-31 14:56:37 +0800

    feat:  1114. 按序打印

---
+++ src/main/java/com/acm/lico/doNotKnow/Foo1114.java
@@ -0,0 +1,92 @@
+package com.acm.lico.doNotKnow;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Foo1114 class
+ *1114. 按序打印
+ * https://leetcode.cn/problems/print-in-order/
+ * 给你一个类：
+ *
+ * public class Foo {
+ *   public void first() { print("first"); }
+ *   public void second() { print("second"); }
+ *   public void third() { print("third"); }
+ * }
+ * 三个不同的线程 A、B、C 将会共用一个 Foo 实例。
+ *
+ * 线程 A 将会调用 first() 方法
+ * 线程 B 将会调用 second() 方法
+ * 线程 C 将会调用 third() 方法
+ * 请设计修改程序，以确保 second() 方法在 first() 方法之后被执行，third() 方法在 second() 方法之后被执行。
+ *
+ * 提示：
+ *
+ * 尽管输入中的数字似乎暗示了顺序，但是我们并不保证线程在操作系统中的调度顺序。
+ * 你看到的输入格式主要是为了确保测试的全面性。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出："firstsecondthird"
+ * 解释：
+ * 有三个线程会被异步启动。输入 [1,2,3] 表示线程 A 将会调用 first() 方法，线程 B 将会调用 second() 方法，线程 C 将会调用 third() 方法。正确的输出是 "firstsecondthird"。
+ *
+ * @author 谢小平
+ * @date 2022/5/30
+ *
+ */
+
+/**
+ 1 复杂度分析
+估算问题中复杂度的上限和下限
+ 时间复杂度
+ 空间复杂度
+    O(1) 一个常量下完成
+    O(n) 一次遍历
+    O(logn) 折半查询
+    O(n^2) 两重嵌套循环查询
+ 2 定位问题
+根据问题类型，确定采用何种算法思维。
+    例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+ 3 数据操作分析
+    根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+ 4 编码实现
+ 5 执行结果
+ 执行结果： 通过 显示详情 添加备注 执行用时： 7 ms , 在所有 Java 提交中击败了 99.98% 的用户
+ 内存消耗： 40.8 MB , 在所有 Java 提交中击败了 20.50% 的用户
+ 通过测试用例： 36 / 36
+ */
+public class Foo1114 {
+    Semaphore semaphoreA = new Semaphore(0);
+    Semaphore semaphoreB = new Semaphore(0);
+    public Foo1114() {
+
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        semaphoreA.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        semaphoreA.acquire();
+        printSecond.run();
+        semaphoreB.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+
+        // printThird.run() outputs "third". Do not change or remove this line.
+        semaphoreB.acquire();
+        printThird.run();
+    }
+}
