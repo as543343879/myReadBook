df59ea7378957e205aad6a196b78b7d252816a19
xiexiaoping <xiexiaoping@lizhi.fm>
2022-06-08 14:56:07 +0800

    feat: 160. 相交链表

---
+++ src/main/java/com/acm/lico/linkedList/Solution160.java
@@ -0,0 +1,93 @@
+package com.acm.lico.linkedList;
+
+/**
+ * Solution160 class
+ *160. 相交链表
+ * https://leetcode.cn/problems/intersection-of-two-linked-lists/
+ * 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ *
+ * 图示两个链表在节点 c1 开始相交：
+ *
+ *
+ *
+ * 题目数据 保证 整个链式结构中不存在环。
+ *
+ * 注意，函数返回结果后，链表必须 保持其原始结构 。
+ *
+ * 自定义评测：
+ *
+ * 评测系统 的输入如下（你设计的程序 不适用 此输入）：
+ *
+ * intersectVal - 相交的起始节点的值。如果不存在相交节点，这一值为 0
+ * listA - 第一个链表
+ * listB - 第二个链表
+ * skipA - 在 listA 中（从头节点开始）跳到交叉节点的节点数
+ * skipB - 在 listB 中（从头节点开始）跳到交叉节点的节点数
+ * 评测系统将根据这些输入创建链式数据结构，并将两个头节点 headA 和 headB 传递给你的程序。如果程序能够正确返回相交节点，那么你的解决方案将被 视作正确答案 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 输出：Intersected at '8'
+ * 解释：相交节点的值为 8 （注意，如果两个链表相交则不能为 0）。
+ * 从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,6,1,8,4,5]。
+ * 在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+ * @author 谢小平
+ * @date 2022/6/8
+ */
+/**
+ 1 复杂度分析
+估算问题中复杂度的上限和下限
+ 时间复杂度
+ 空间复杂度
+    O(1) 一个常量下完成
+    O(n) 一次遍历
+    O(logn) 折半查询
+    O(n^2) 两重嵌套循环查询
+ 2 定位问题
+根据问题类型，确定采用何种算法思维。
+ 双指针法。
+ hash表法
+ * 输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+ * 假如 A 是 第三个节点
+ * 接入 B 是 第四个节点
+ *  t % 6 == 3 在交点
+ *  t % 7 == 4 在交点 t == 39 的时候 满足运算。
+    例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+ 3 数据操作分析
+    根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+ 4 编码实现
+ 5 执行结果
+ 执行结果： 通过 显示详情 添加备注
+ 执行用时： 316 ms , 在所有 Java 提交中击败了 5.75% 的用户
+ 内存消耗： 44.3 MB , 在所有 Java 提交中击败了 43.24% 的用户
+ 通过测试用例： 39 / 39
+ */
+public class Solution160 {
+    /**
+
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA = headA;
+        ListNode tempB = headB;
+        int count = 0;
+        while (tempA != tempB) {
+            count++;
+            tempA = tempA == null ? headA : tempA.next;
+            tempB = tempB == null ? headB : tempB.next;
+        }
+        System.out.println(count);
+        return tempA;
+    }
+}
