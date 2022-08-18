a9964e3d110f5089e27fc4915785a73b4de2f4a6
xiexiaoping <xiexiaoping@lizhi.fm>
2022-08-18 14:03:32 +0800

    feat:  412. Fizz Buzz

---
+++ src/main/java/com/acm/lico/doNotKnow/Solution412.java
@@ -0,0 +1,37 @@
+package com.acm.lico.doNotKnow;
+
+import java.util.List;
+
+/**
+ * Solution412 class
+ * https://leetcode.cn/problems/fizz-buzz/
+ *412. Fizz Buzz
+ * 给你一个整数 n ，找出从 1 到 n 各个整数的 Fizz Buzz 表示，并用字符串数组 answer（下标从 1 开始）返回结果，其中：
+ *
+ * answer[i] == "FizzBuzz" 如果 i 同时是 3 和 5 的倍数。
+ * answer[i] == "Fizz" 如果 i 是 3 的倍数。
+ * answer[i] == "Buzz" 如果 i 是 5 的倍数。
+ * answer[i] == i （以字符串形式）如果上述条件全不满足。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：n = 3
+ * 输出：["1","2","Fizz"]
+ * 示例 2：szdfcgbhnjmk,.l;/"
+ * `    ]
+ *
+ * 输入：n = 5
+ * 输出：["1","2","Fizz","4","Buzz"]
+ * 示例 3：
+ *
+ * 输入：n = 15
+ * 输出：["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ * @author 谢小平
+ * @date 2022/8/18
+ */
+public class Solution412 {
+    public List<String> fizzBuzz(int n) {
+
+    }
+}
