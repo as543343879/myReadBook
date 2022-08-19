4393b3ab89dc0a3333c2bac2ce64b180917406e6
xiexiaoping <xiexiaoping@lizhi.fm>
2022-08-19 12:42:57 +0800

    feat:  412. Fizz Buzz

--- src/main/java/com/acm/lico/doNotKnow/Solution412.java
+++ src/main/java/com/acm/lico/doNotKnow/Solution412.java
@@ -1,5 +1,6 @@
 package com.acm.lico.doNotKnow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -30,8 +31,57 @@ import java.util.List;
  * @author 谢小平
  * @date 2022/8/18
  */
-public class Solution412 {
-    public List<String> fizzBuzz(int n) {
+/**
+ 1 复杂度分析
+估算问题中复杂度的上限和下限
 
+ 时间复杂度
+ O(N)
+ 空间复杂度
+ O(1) 返回值 不计算到空间复杂度里面
+ // 错误
+ O(N)
+    O(1) 一个常量下完成
+    O(n) 一次遍历
+    O(logn) 折半查询
+    O(n^2) 两重嵌套循环查询
+ 2 定位问题
+根据问题类型，确定采用何种算法思维。
+ 模拟题：
+
+    例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+ 3 数据操作分析
+    根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+ 4 编码实现
+ 5 执行结果
+ 执行结果： 通过 显示详情 添加备注
+ 执行用时： 1 ms , 在所有 Java 提交中击败了 99.92% 的用户
+ 内存消耗： 42.2 MB , 在所有 Java 提交中击败了 89.30% 的用户
+ 通过测试用例： 8 / 8
+ */
+public class Solution412 {
+
+    public List<String> fizzBuzz(int n) {
+        String fizzBuzz35 = "FizzBuzz";
+        String fizz3 = "Fizz";
+        String buzz5 = "Buzz";
+        List<String> listRes = new ArrayList<>();
+        for(int i =1; i <= n; i ++){
+            int i3 = i % 3;
+            int i5 = i % 5;
+            if(i3 ==0 && i5 == 0) {
+                listRes.add(fizzBuzz35);
+            } else if(i3 == 0) {
+                listRes.add(fizz3);
+            } else if(i5 == 0) {
+                listRes.add(buzz5);
+            } else {
+                listRes.add(String.valueOf(i));
+            }
+        }
+        return listRes;
     }
 }
