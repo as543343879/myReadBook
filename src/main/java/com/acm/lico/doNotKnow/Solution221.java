05976d000db4fc628925ac2d896cfe5c5587e8cd
xiexiaoping <xiexiaoping@lizhi.fm>
2022-08-23 21:26:20 +0800

    feat:221. 最大正方形

---
+++ src/main/java/com/acm/lico/doNotKnow/Solution221.java
@@ -0,0 +1,32 @@
+package com.acm.lico.doNotKnow;
+
+/**
+ * Solution221 class
+ * https://leetcode.cn/problems/maximal-square/
+ *221. 最大正方形
+ * 在一个由 '0' 和 '1' 组成的二维矩阵内，找到只包含 '1' 的最大正方形，并返回其面积。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：4
+ * 示例 2：
+ *
+ *
+ * 输入：matrix = [["0","1"],["1","0"]]
+ * 输出：1
+ * 示例 3：
+ *
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ * @author 谢小平
+ * @date 2022/8/23
+ */
+public class Solution221 {
+    public int maximalSquare(char[][] matrix) {
+
+    }
+}
