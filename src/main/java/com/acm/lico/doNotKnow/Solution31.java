e9b31f5e6feb6596b4d97e82fbfe4d98cfcb1104
xiexiaoping <xiexiaoping@lizhi.fm>
2022-09-02 13:00:20 +0800

    feat: 31 下一个排列

---
+++ src/main/java/com/acm/lico/doNotKnow/Solution31.java
@@ -0,0 +1,112 @@
+package com.acm.lico.doNotKnow;
+
+//整数数组的一个 排列 就是将其所有成员以序列或线性顺序排列。
+//
+// 
+// 例如，arr = [1,2,3] ，以下这些都可以视作 arr 的排列：[1,2,3]、[1,3,2]、[3,1,2]、[2,3,1] 。 
+// 
+//
+// 整数数组的 下一个排列 是指其整数的下一个字典序更大的排列。更正式地，如果数组的所有排列根据其字典顺序从小到大排列在一个容器中，那么数组的 下一个排列 就
+//是在这个有序容器中排在它后面的那个排列。如果不存在下一个更大的排列，那么这个数组必须重排为字典序最小的排列（即，其元素按升序排列）。 
+//
+// 
+// 例如，arr = [1,2,3] 的下一个排列是 [1,3,2] 。 
+// 类似地，arr = [2,3,1] 的下一个排列是 [3,1,2] 。 
+// 而 arr = [3,2,1] 的下一个排列是 [1,2,3] ，因为 [3,2,1] 不存在一个字典序更大的排列。 
+// 
+//
+// 给你一个整数数组 nums ，找出 nums 的下一个排列。 
+//
+// 必须 原地 修改，只允许使用额外常数空间。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,2,3]
+//输出：[1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,1]
+//输出：[1,2,3]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [1,1,5]
+//输出：[1,5,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 100 
+// 0 <= nums[i] <= 100 
+// 
+//
+// Related Topics 数组 双指针 👍 1882 👎 0
+
+/**
+ 1 复杂度分析
+ 估算问题中复杂度的上限和下限
+ 时间复杂度
+ 空间复杂度
+ O(1) 一个常量下完成
+ O(n) 一次遍历
+ O(logn) 折半查询
+ O(n^2) 两重嵌套循环查询
+ 2 定位问题
+ 根据问题类型，确定采用何种算法思维。
+ 
+ 
+ 例如
+ 这个问题是什么类型（排序、查找、最优化）的问题；
+ 这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+ 采用哪些数据结构或算法思维，能把这个问题解决。
+ 3 数据操作分析
+ 根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+ 4 编码实现
+ 5 执行结果
+ 解答成功:
+ 执行耗时:0 ms,击败了100.00% 的Java用户
+ 内存消耗:42.1 MB,击败了6.04% 的Java用户
+ */
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution31 {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while ( i >= 0 && nums[i] >= nums[i +1]) {
+            i --;
+        }
+        if(i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i] ) {
+                j --;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums, i + 1);
+    }
+    public void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public void reverse(int[] nums, int left) {
+        int right = nums.length -1;
+        while (left < right) {
+            swap(nums,left,right);
+            left ++;
+            right --;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
