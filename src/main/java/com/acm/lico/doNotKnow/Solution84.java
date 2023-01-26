89a60f1652d1c5b74c7e79a0b4fd6ab416dd6639
xiexiaoping <543343879@qq.com>
2023-01-26 22:35:04 +0800

    feat:  84 ,柱状图中最大的矩形

--- src/main/java/com/acm/lico/doNotKnow/Solution84.java
+++ src/main/java/com/acm/lico/doNotKnow/Solution84.java
@@ -39,6 +39,7 @@ package com.acm.lico.doNotKnow; /**
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 
 /**
  1 复杂度分析
@@ -108,6 +109,75 @@ class Solution84 {
                 res = Math.max(res, height * width);
             }
             stack.addLast(i);
+        }
+        return res;
+    }
+
+    /**
+     * 单调栈确定左右边界。 每跟柱子的影响返回 * 高度。
+     * 每根柱子的影响范围 ，应该是左右两侧，第一根小于当前柱子的存在。
+     *
+     *
+     * 时间
+     * 29 ms
+     * 击败
+     * 49.50%
+     * 内存
+     * 53.5 MB
+     * 击败
+     * 73.59%
+     *
+     * @param heights
+     * @return
+     */
+    /**
+     1 复杂度分析
+    估算问题中复杂度的上限和下限
+     时间复杂度 O(N)
+     空间复杂度 O(N)
+        O(1) 一个常量下完成
+        O(n) 一次遍历
+        O(logn) 折半查询
+        O(n^2) 两重嵌套循环查询
+     2 定位问题
+    根据问题类型，确定采用何种算法思维。
+        例如
+         这个问题是什么类型（排序、查找、最优化）的问题；
+         这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+         采用哪些数据结构或算法思维，能把这个问题解决。
+     3 数据操作分析
+        根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+     4 编码实现
+     5 执行结果
+     */
+    public int largestRectangleArea3(int[] heights) {
+        int res = 0;
+        if(heights.length == 0) {
+            return 0;
+        }
+        LinkedList<Integer> stack  = new LinkedList<>();
+        int n = heights.length;
+        int[] leftIndex = new int[n];
+        int[] rightIndex = new int[n];
+        for(int i = 0; i < n; i ++) {
+            while ( (!stack.isEmpty()  && heights[stack.peek()] >= heights[i] )) {
+                stack.pop();
+            }
+            leftIndex[i] = stack.isEmpty() ? -1: stack.peek();
+            stack.push(i);
+        }
+        stack.clear();
+
+        for(int i = n - 1; i >= 0; i --) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            rightIndex[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        for(int i = 0; i < n; i ++) {
+            res = Math.max(heights[i] * (rightIndex[i] - leftIndex[i] - 1), res);
         }
         return res;
     }
