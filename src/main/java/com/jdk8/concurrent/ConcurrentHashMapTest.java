586b96f5ea94e8946da9127318a9da8055ac6db8
xiexiaoping <xiexiaoping@lizhi.fm>
2023-03-01 21:14:44 +0800

    feat:   2 ,两数相加

---
+++ src/main/java/com/jdk8/concurrent/ConcurrentHashMapTest.java
@@ -0,0 +1,18 @@
+package com.jdk8.concurrent;
+
+import org.junit.Test;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * ConcurrentHashMapTest class
+ *
+ * @author 谢小平
+ * @date 2023/3/1
+ */
+public class ConcurrentHashMapTest {
+    @Test
+    public void mainTest() {
+        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<String,String>();
+    }
+}
