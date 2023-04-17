e57064915776af03ba6babf1f8d2a18fbb60dac4
xiexiaoping <xiexiaoping@lizhi.fm>
2023-04-17 09:31:35 +0800

    feat:  169 ,多数元素

--- src/main/java/com/acm/lico/doNotKnow/Solution169.java
+++ src/main/java/com/acm/lico/doNotKnow/Solution169.java
@@ -1,4 +1,4 @@
-/**
+package com.acm.lico.doNotKnow; /**
  169 ,多数元素
  //给定一个大小为 n 的数组 nums ，返回其中的多数元素。多数元素是指在数组中出现次数 大于 ⌊ n/2 ⌋ 的元素。
  //
@@ -72,7 +72,7 @@ import java.util.Map;
  内存消耗:44.9 MB,击败了55.58% 的Java用户
  */
 //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
+class Solution169 {
     public int majorityElement(int[] nums) {
         if(nums == null) {
             return  0;
