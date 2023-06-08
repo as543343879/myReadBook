d9b80e0b265919a1c58b4ab464f33a62c59da298
xiexiaoping <xiexiaoping@lizhi.fm>
2023-06-08 09:59:00 +0800

    feat:  4 ,寻找两个正序数组的中位数

--- src/main/java/com/acm/lico/sort/Solution4.java
+++ src/main/java/com/acm/lico/sort/Solution4.java
@@ -1,45 +1,45 @@
 package com.acm.lico.sort; /**
- 4 ,寻找两个正序数组的中位数
- //给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
- //
- // 算法的时间复杂度应该为 O(log (m+n)) 。
- //
- //
- //
- // 示例 1：
- //
- //
- //输入：nums1 = [1,3], nums2 = [2] , 3 / 2 = 1
- //输出：2.00000
- //解释：合并数组 = [1,2,3] ，中位数 2
- //
- //
- // 示例 2：
- //
- //
- //输入：nums1 = [1,2], nums2 = [3,4] ,4 / 2 = 2
- //输出：2.50000
- //解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
- //
- //
- //
- //
- //
- //
- // 提示：
- //
- //
- // nums1.length == m
- // nums2.length == n
- // 0 <= m <= 1000
- // 0 <= n <= 1000
- // 1 <= m + n <= 2000
- // -10⁶ <= nums1[i], nums2[i] <= 10⁶
- //
- //
- // Related Topics 数组 二分查找 分治 👍 6312 👎 0
-
+ * 4 ,寻找两个正序数组的中位数
+ * //给定两个大小分别为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的 中位数 。
+ * //
+ * // 算法的时间复杂度应该为 O(log (m+n)) 。
+ * //
+ * //
+ * //
+ * // 示例 1：
+ * //
+ * //
+ * //输入：nums1 = [1,3], nums2 = [2] , 3 / 2 = 1
+ * //输出：2.00000
+ * //解释：合并数组 = [1,2,3] ，中位数 2
+ * //
+ * //
+ * // 示例 2：
+ * //
+ * //
+ * //输入：nums1 = [1,2], nums2 = [3,4] ,4 / 2 = 2
+ * //输出：2.50000
+ * //解释：合并数组 = [1,2,3,4] ，中位数 (2 + 3) / 2 = 2.5
+ * //
+ * //
+ * //
+ * //
+ * //
+ * //
+ * // 提示：
+ * //
+ * //
+ * // nums1.length == m
+ * // nums2.length == n
+ * // 0 <= m <= 1000
+ * // 0 <= n <= 1000
+ * // 1 <= m + n <= 2000
+ * // -10⁶ <= nums1[i], nums2[i] <= 10⁶
+ * //
+ * //
+ * // Related Topics 数组 二分查找 分治 👍 6312 👎 0
  **/
+
 /**
  1 复杂度分析
  估算问题中复杂度的上限和下限
@@ -76,24 +76,123 @@ package com.acm.lico.sort; /**
  */
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution4 {
+    /**
+     1 复杂度分析
+     估算问题中复杂度的上限和下限
+     时间复杂度  O(logN) 错误， 应该是 O(N)
+     空间复杂度  O(1)
+     O(1) 一个常量下完成
+     O(n) 一次遍历
+     O(logn) 折半查询
+     O(n^2) 两重嵌套循环查询
+     2 定位问题
+     根据问题类型，确定采用何种算法思维。
+     例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+     思路：
+     择半查找 定位分割线。 假如 长度 分别位 n , m 。 假如 下标 i，j 分别满足 i + j = n - i + m - j并且  s[i-1] < s2[j] & s2[j-1] < s[i]
+     如果是len 是奇数 res = max(s[i-1], s2[j-1])
+     如果len 是偶数 res = (max(s[i-1], s2[j-1]) + min(s[i], s[j-1]) * 1.0 / 2.9;
+     3 数据操作分析
+     根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+     4 编码实现
+     5 执行结果
+     解答成功:
+     执行耗时:1 ms,击败了100.00% 的Java用户
+     内存消耗:43.5 MB,击败了17.71% 的Java用户
+     */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int len = len1 + len2;
+        boolean even = len % 2 == 0;
+        int targetIndex = (len + 1) / 2;
+        if (even) {
+            targetIndex += 1;
+        }
+
+        int i = 0, j = 0;
+        int count = 0;
+        boolean isI = true;
+        boolean isPreI = true;
+        while (i < len1 || j < len2) {
+            isPreI = isI;
+            if (i >= len1) {
+                j++;
+                isI = false;
+            } else if (j >= len2) {
+                i++;
+                isI = true;
+            } else {
+                if (nums1[i] < nums2[j]) {
+                    i++;
+                    isI = true;
+                } else {
+                    j++;
+                    isI = false;
+                }
+            }
+
+            if (i + j == targetIndex) {
+                break;
+            }
+        }
+
+        if (even) {
+            if (isI == isPreI) {
+                if (isI) {
+                    return (nums1[i - 2] + nums1[i - 1]) * 1.0 / 2;
+                } else {
+                    return (nums2[j - 2] + nums2[j - 1]) * 1.0 / 2;
+                }
+            } else {
+                return (nums1[i - 1] + nums2[j - 1]) * 1.0 / 2;
+            }
+        } else {
+            return (isI ? nums1[i - 1] : nums2[j - 1]) * 1.0;
+        }
+
+
+    }
+
+    public double findMedianSortedArraysAbstraction(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || A[aStart] < B[bStart])) {
+                right = A[aStart++];
+            } else {
+                right = B[bStart++];
+            }
+        }
+        if ((len & 1) == 0) return (left + right) / 2.0;
+        else return right;
+    }
+
+    public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
         int n1 = nums1.length + nums2.length;
 
         int target1, target2;
-        if(n1 % 2 == 0) {
+        if (n1 % 2 == 0) {
             target2 = n1 / 2;
             target1 = target2 - 1;
         } else {
             target1 = n1 / 2;
             target2 = target1;
         }
-        if(nums1.length == 0 && nums2.length == 0 ) {
+        if (nums1.length == 0 && nums2.length == 0) {
             return 0D;
         }
-        if(nums1.length == 0) {
+        if (nums1.length == 0) {
             return (nums2[target1] * 1.0 + nums2[target2] * 1.0) / 2;
         }
-        if(nums2.length == 0) {
+        if (nums2.length == 0) {
             return (nums1[target1] * 1.0 + nums1[target2] * 1.0) / 2;
         }
         int count = 0;
@@ -102,26 +201,26 @@ class Solution4 {
 
         int targetValue1 = 0;
         int targetValue2 = 0;
-        while ( count <= target2) {
-            if( (j >= nums2.length) || ( i < nums1.length && nums1[i] <= nums2[j]) ) {
-                if(count == target1) {
+        while (count <= target2) {
+            if ((j >= nums2.length) || (i < nums1.length && nums1[i] <= nums2[j])) {
+                if (count == target1) {
                     targetValue1 = nums1[i];
                 }
-                if(count == target2) {
+                if (count == target2) {
                     targetValue2 = nums1[i];
                 }
-                i ++;
+                i++;
             } else {
-                if(count == target1) {
+                if (count == target1) {
                     targetValue1 = nums2[j];
                 }
-                if(count == target2) {
+                if (count == target2) {
                     targetValue2 = nums2[j];
                 }
-                j ++;
+                j++;
 
             }
-            count ++;
+            count++;
         }
         return (targetValue1 * 1.0 + targetValue2 * 1.0) / 2;
     }
@@ -133,7 +232,7 @@ class Solution4 {
      * @return
      */
     public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
-        if(nums1.length > nums2.length) {
+        if (nums1.length > nums2.length) {
             int[] t = nums1;
             nums1 = nums2;
             nums2 = t;
@@ -147,10 +246,10 @@ class Solution4 {
             int i = left + (right - left + 1) / 2;
             int j = leftTotal - i;
             // [left ,i - 1]
-            if(nums1[i - 1] > nums2[j]) {
+            if (nums1[i - 1] > nums2[j]) {
                 right = i - 1;
                 // [i,right]
-            } else  {
+            } else {
                 // [left(i),right] 两个元素的时候 左边界不会在小，死循环。 让  i = left + (right - left) / 2;  加1
                 left = i;
             }
@@ -158,14 +257,14 @@ class Solution4 {
         int i = left;
         int j = leftTotal - i;
         int num1LeftMax = i == 0 ? Integer.MIN_VALUE : nums1[i - 1];
-        int num1rightMin = i == n1 ? Integer.MAX_VALUE :  nums1[i];
+        int num1rightMin = i == n1 ? Integer.MAX_VALUE : nums1[i];
         int nmu2leftMax = j == 0 ? Integer.MIN_VALUE : nums2[j - 1];
         int num2rightMin = j == n2 ? Integer.MAX_VALUE : nums2[j];
         // 奇数
-        if( (n1 + n2) % 2 == 1) {
-            return Math.max(num1LeftMax,nmu2leftMax);
+        if ((n1 + n2) % 2 == 1) {
+            return Math.max(num1LeftMax, nmu2leftMax);
         } else {
-            return (double)  (Math.max(num1LeftMax,nmu2leftMax) + Math.min(num1rightMin,num2rightMin) ) / 2.0;
+            return (double) (Math.max(num1LeftMax, nmu2leftMax) + Math.min(num1rightMin, num2rightMin)) / 2.0;
         }
     }
 }
