850fa642f0cf3c83c6b798e8bbc3aa5b5265b468
xiexiaoping <xiexiaoping@lizhi.fm>
2023-08-19 15:46:12 +0800

    feat:  2235 ,两整数相加

---
+++ src/main/java/com/acm/lico/largeAmount/Solution2235.java
@@ -0,0 +1,43 @@
+package com.acm.lico.largeAmount;
+
+/**
+ 2235 ,两整数相加
+ //给你两个整数 num1 和 num2，返回这两个整数的和。
+ //
+ //
+ //
+ // 示例 1：
+ //
+ //
+ //输入：num1 = 12, num2 = 5
+ //输出：17
+ //解释：num1 是 12，num2 是 5 ，它们的和是 12 + 5 = 17 ，因此返回 17 。
+ //
+ //
+ // 示例 2：
+ //
+ //
+ //输入：num1 = -10, num2 = 4
+ //输出：-6
+ //解释：num1 + num2 = -6 ，因此返回 -6 。
+ //
+ //
+ //
+ //
+ // 提示：
+ //
+ //
+ // -100 <= num1, num2 <= 100
+ //
+ //
+ // Related Topics 数学 👍 173 👎 0
+
+ **/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution2235 {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
