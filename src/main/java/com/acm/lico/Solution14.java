8785c3f41e4fe95a6e289fbaf7a94cbb1c09fd1c
xiexiaoping <543343879@qq.com>
2023-10-05 13:06:38 +0800

    feat:  14 ,最长公共前缀

--- src/main/java/com/acm/lico/Solution14.java
+++ src/main/java/com/acm/lico/Solution14.java
@@ -70,6 +70,55 @@ package com.acm.lico; /**
  */
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution14 {
+    /**
+     1 复杂度分析
+     估算问题中复杂度的上限和下限
+     时间复杂度  O(N*M)
+     空间复杂度  O(1)
+     O(1) 一个常量下完成
+     O(n) 一次遍历
+     O(logn) 折半查询
+     O(n^2) 两重嵌套循环查询
+     2 定位问题
+     根据问题类型，确定采用何种算法思维。
+     例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+     思路：
+     先从第一个字符串遍历。res 存储结果
+     如果 str[0][i]，等后面字符串都相等， res + str[0][i].
+     返回res
+     3 数据操作分析
+     根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+     4 编码实现
+     5 执行结果
+     解答成功:
+     执行耗时:1 ms,击败了70.62% 的Java用户
+     内存消耗:39.2 MB,击败了57.97% 的Java用户
+     */
+    public String longestCommonPrefixNew2(String[] strs) {
+        StringBuilder sbRes = new StringBuilder();
+        if(strs.length > 0) {
+            String strLine1 = strs[0];
+            for(int i = 0; i < strLine1.length(); i ++) {
+                char t = strLine1.charAt(i);
+                boolean oneRes = true;
+                for(int l = 1; l < strs.length; l ++) {
+                    if((strs[l].length() <= i) || ( t != strs[l].charAt(i))) {
+                        oneRes = false;
+                        break;
+                    }
+                }
+                if(oneRes) {
+                    sbRes.append(t);
+                } else {
+                    break;
+                }
+            }
+        }
+        return sbRes.toString();
+    }
     public String longestCommonPrefix(String[] strs) {
         if(strs.length == 0) {
             return "";
