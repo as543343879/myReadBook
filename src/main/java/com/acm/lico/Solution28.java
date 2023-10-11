cf738a0e36fec2869a08c136301780797f876df8
xiexiaoping <543343879@qq.com>
2023-10-11 15:35:38 +0800

    feat:  28. 实现 strStr()

--- src/main/java/com/acm/lico/Solution28.java
+++ src/main/java/com/acm/lico/Solution28.java
@@ -39,4 +39,48 @@ public class Solution28 {
 
         return -1;
     }
+
+
+    /**
+     1 复杂度分析
+     估算问题中复杂度的上限和下限
+     时间复杂度  O(N*M)
+     空间复杂度  O(1)
+     O(1) 一个常量下完成
+     O(n) 一次遍历
+     O(logn) 折半查询
+     O(n^2) 两重嵌套循环查询
+     2 定位问题
+     根据问题类型，确定采用何种算法思维。
+     例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+     思路：
+     1. 循环遍历 haystack， 找到匹配 needle 的。
+     3 数据操作分析
+     根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+     4 编码实现
+     5 执行结果
+     解答成功:
+     执行耗时:1 ms,击败了40.96% 的Java用户
+     内存消耗:39.2 MB,击败了82.04% 的Java用户
+     */
+    public int strStrNew2(String haystack, String needle) {
+        int needleLen = needle.length();
+        for(int i = 0; i <= haystack.length() - needleLen; i ++  ) {
+
+            boolean tempRes = true;
+            for(int j = 0; j <needleLen; j ++) {
+                if(needle.charAt(j) != haystack.charAt(i + j) ) {
+                    tempRes = false;
+                    break;
+                }
+            }
+            if(tempRes) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
