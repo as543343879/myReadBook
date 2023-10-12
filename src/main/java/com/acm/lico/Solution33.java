b2fce70f2e1bec9310510a439946bfd42456a2be
xiexiaoping <543343879@qq.com>
2023-10-12 22:57:36 +0800

    feat:  33. 搜索旋转排序数组

--- src/main/java/com/acm/lico/Solution33.java
+++ src/main/java/com/acm/lico/Solution33.java
@@ -47,4 +47,78 @@ public class Solution33 {
 
         return -1;
     }
+
+    /**
+     1 复杂度分析
+     估算问题中复杂度的上限和下限
+     时间复杂度 O(logN)
+     空间复杂度 O(logN)
+     O(1) 一个常量下完成
+     O(n) 一次遍历
+     O(logn) 折半查询
+     O(n^2) 两重嵌套循环查询
+     2 定位问题
+     根据问题类型，确定采用何种算法思维。
+     例如
+     这个问题是什么类型（排序、查找、最优化）的问题；
+     这个问题的复杂度下限是多少，即最低的时间复杂度可能是多少；
+     采用哪些数据结构或算法思维，能把这个问题解决。
+     思路： 二分查找 [4,5,6,7,0,1,2], target = 3
+     1. 选取中间数 middle。 如果 target == middle 返回。
+     2. 如果 left 和 middle 都大于 targe, 从 middle + 1 到 right找， 否则从 左边找。 思路错误。
+
+     思路看了官方代码： 二分查找， 因为中位数左右两边 总有一边是有序的， 根据这个有序的，就能用排除法 缺点给边界
+     1. 选取中位数，
+     2. 如果 nums[left] < nums[middle] 左边有序， 根据左边排除
+     - 如果 targe  小于 nums[left] 并且 小于  nums[middle] 。 从左边找
+     - 否则 从右边
+     3. 否则
+     - 如果 tager 大于 nums[right] 并且小于 nums[middle], 从右边找
+     - 否则从左边找
+
+     3 数据操作分析
+     根据增、删、查和数据顺序关系去选择合适的数据结构，利用空间换取时间。
+
+     4 编码实现
+     5 执行结果
+     解答成功:
+     执行耗时:0 ms,击败了100.00% 的Java用户
+     内存消耗:39.7 MB,击败了71.20% 的Java用户
+     */
+    public int searchNew20(int[] nums, int target) {
+        return findMiddle(nums, 0, nums.length - 1, target);
+    }
+
+    private int findMiddle(int[] nums, int left, int right,int target) {
+        if(left > right) {
+            return -1;
+        }
+        // 加一 防止出现 只有两个数的时候. middle = 1， 然后结果又在 0到1 一直循环
+        int middle =  (right + left +1) / 2;
+        if(nums[middle] == target) {
+            return middle;
+        }
+//        2. 如果 nums[left] < nums[middle] 左边有序， 根据左边排除
+//                - 如果 targe  小于 nums[left] 并且 小于  nums[middle] 。 从左边找
+//                - 否则 从右边
+//        3. 否则
+//                - 如果 tager 大于 nums[right] 并且小于 nums[middle], 从右边找
+//                - 否则从左边找
+        if(nums[left] < nums[middle]) {
+            if(target >= nums[left] && target < nums[middle] ) {
+                return  findMiddle(nums, left, middle-1, target);
+            } else {
+                return findMiddle(nums, middle + 1, right, target);
+            }
+        } else {
+            if(target > nums[middle] && target <= nums[right]) {
+                return findMiddle(nums, middle + 1, right, target);
+            } else {
+                return  findMiddle(nums, left, middle-1, target);
+            }
+        }
+
+
+
+    }
 }
